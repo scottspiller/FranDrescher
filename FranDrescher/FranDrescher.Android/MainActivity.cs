52b4dea0694d943788d3c6270d10e6a8d6d90454
Scott Spiller <Scott.Spiller@flyfrontier.com>
2019-06-07 16:22:55 -0600

    android splash screen

--- FranDrescher/FranDrescher.Android/MainActivity.cs
+++ FranDrescher/FranDrescher.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.OS;
 
 namespace FranDrescher.Droid
 {
-    [Activity(Label = "FranDrescher", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FranDrescher", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
